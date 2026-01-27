71b7457f551ee8494df31fec1c389ebe3fd1ae6b
Borna <zamoracborna@gmail.com>
2026-01-27 20:56:36 +0100

    vjezba01

---
+++ 2_JS/vjezba01/skripta.js
@@ -0,0 +1,22 @@
+console.info('Hello, World 6');
+console.table({ime: "Pero", prezime: "Peric"});
+
+document.getElementById('ispis').innerText = 'Hello, World 7';
+document.getElementById('ispis').style.color = 'red';
+
+document.getElementById('ue').innerHTML = '<a href="#">Hello 8</a>';
+
+console.log('Osnovna skola "Ivana Filipovica"');
+
+console.log('Mc\'Donalds');
+console.log("Mc' Donalds");
+
+
+//sto ako zelim u konzolu ispisati \
+console.log('onda radim \\ ovo')
+
+console.log(`
+    ovo je moj
+    tekst
+    kroz vise redova`
+);
