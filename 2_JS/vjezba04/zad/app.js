248102bdc636983918f81e3c512d91c5683cca55
Borna <zamoracborna@gmail.com>
2026-02-03 21:00:41 +0100

    Vjezba04

---
+++ 2_JS/vjezba04/zad/app.js
@@ -0,0 +1,79 @@
+
+const rezultat = document.getElementById('rezultat');
+document.getElementById('izvedi').addEventListener('click', () => {
+  rezultat.innerHTML = '';
+  const a = document.getElementById('a').value;
+  const b = document.getElementById('b').value;
+  const c = document.getElementById('c').value;
+  const d = document.getElementById('d').value;
+  const zadatak = document.getElementById('zadatak').value;
+
+  // Za svaki zadatak zahtijevamo određene ulaze
+  if (zadatak === '1') {
+   // console.log('1. zadatak');
+   // console.log(a);
+   
+   //ovdje rjesavam zad 1
+   const x= Number(a)
+   if(!x && x!=0){
+    rezultat.innerHTML='Unesite broj u polje A'
+    return;
+   }
+
+   const y= Number(b)
+   if(!y && y!=0){
+     rezultat.innerHTML='Unesite broj u polje B'
+     return;
+    }
+
+   if(x>y){
+      rezultat.innerHTML=x
+    } else if(y>x){
+      rezultat.innerHTML = y
+    } 
+    else{
+      rezultat.innerHTML='Brojevi su jednaki'
+    }
+    return;
+  }
+//kraj zad 1
+
+  if (zadatak === '2') {
+    const x= Number(a);
+    const y= Number(b);
+    const z= Number(c);
+
+   if(!x && x!=0){
+    rezultat.innerHTML='Unesite broj u polje A'
+    return;
+   }
+
+    if(!y && y!=0){
+    rezultat.innerHTML='Unesite broj u polje B'
+    return;
+   }
+
+    if(!z && z!=0){
+    rezultat.innerHTML='Unesite broj u polje C'
+    return;
+   }
+
+   if(x<y && x<z){
+    rezultat.innerHTML=x
+   } else if(y<x && y<z){
+    rezultat.innerHTML=y
+   } else {
+    rezultat.innerHTML=z
+   }
+    return;
+  }
+  //kraj zad 2
+
+  
+  
+  // ovo će se ispisati ako u HTML dodatke option za zadatak a ovdje ga ne obradite
+  rezultat.innerHTML = `Nepoznati zadatak ${zadatak}`;
+});
+
+
+
