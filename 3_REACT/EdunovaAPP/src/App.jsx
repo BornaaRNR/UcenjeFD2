ade3bde56a4c8cac8ac8c77036e1676bb674dc0b
Borna Novak <zamoracborna@gmail.com>
2026-03-13 19:42:17 +0100

    Predlozak aplikacije

--- 3_REACT/EdunovaAPP/src/App.jsx
+++ 3_REACT/EdunovaAPP/src/App.jsx
@@ -1,11 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { Container } from 'react-bootstrap'
+import Izbornik from './components/Izbornik'
+import { Route, Routes } from 'react-router-dom'
+import { RouteNames } from './constants'
+import SmjerPregled from './pages/smjerovi/SmjerPregled'
+import Home from './pages/Home'
 
 function App() {
 
   return (
-    <>
-    Hello Edunova
-    </>
+    <Container>
+      <Izbornik />
+      <Routes>
+        <Route path={RouteNames.HOME} element={<Home />} />
+        <Route path={RouteNames.SMJEROVI} element={<SmjerPregled />}/>
+      </Routes>
+      <hr />
+      &copy; Edunova
+    </Container>
   )
 }
 
---
+++ 3_REACT/EdunovaAPP/src/constants.js
@@ -0,0 +1,6 @@
+export const IME_APLIKACIJE = 'EdunovaAPP'
+
+export const RouteNames = {
+    HOME: '/',
+    SMJEROVI: '/smjerovi'
+}
